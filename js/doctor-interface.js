8ea7b922f8b76fe42e00acaa38c710d664e9727a
James Higgins <james.milan.higgins@gmail.com>
2017-05-26 14:28:05 -0700

    add postal code input to index form and api call

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,6 +1,6 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-var displayInfo = function(medicalIssue, doctors) {
+var displayInfo = function(medicalIssue, state, doctors) {
 
   doctors.forEach(function(doctor) {
 
@@ -10,6 +10,7 @@ var displayInfo = function(medicalIssue, doctors) {
     var bio = doctor.profile.bio;
     var image = doctor.profile.image_url;
     // var lat = doctor.practices[0].lat;
+    // console.log(doctor);
     // var lon = doctor.practices[0].lon;
   // $('.output-medical').append("<p class='title'>" + first_name + " " + last_name + ", " +  title + "</p>" + "<p class='bio'>" + bio + "</p>" + "<hr>");
   $('.output-medical').append("<p class='title'>" + first_name + " " + last_name + ", " +  title + "</p>" + "<p class='bio'>" + bio + "</p>" + "<img class='image' src=" + "'" + image + "'" + ">" + "<hr>");
@@ -18,7 +19,6 @@ var displayInfo = function(medicalIssue, doctors) {
   // $('.output-doctor').append("<img class='image' src=" + "'" + image + "'" + ">");
   // $('.output-doctor').append("<img class='image' src="`${doctor.profile.image_url}`" >");
   //
-  // console.log(image);
   // });
 };
 
@@ -28,8 +28,9 @@ $('#doctor-info').submit(function(event) {
   event.preventDefault();
   $('.output-medical').text("");
   var medicalIssue = $('#medical').val();
-  // var state = $('#state').val();
+  var state = $('#state').val();
+  console.log(state);
 
-  myDoctor.getDoctors(medicalIssue, displayInfo);
+  myDoctor.getDoctors(medicalIssue, state, displayInfo);
   });
 });
