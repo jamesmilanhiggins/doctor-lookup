4fe1b915d939b3543c103a3d7a9076d58a3ffb5e
James Higgins <james.milan.higgins@gmail.com>
2017-05-26 10:07:52 -0700

    add doctors attriute and consolue log for each doctor.first_name

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,13 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
-var displayInfo = function(medicalIssue, doctorName, doctors) {
+var displayInfo = function(medicalIssue, doctors) {
   // $('.output-doctor').text(`${medicalIssue} ${docotorName} doctors:`);
   // $('.output-medical').append("<li>" + bike.stolen_location + " " + bike.title + " " + bike.frame_colors + "</li>");
-  // doctors.forEach(function(doctor) {
-  //   var name = doctor.profile.first_name;
-// });
+  doctors.forEach(function(doctor) {
+    var first_name = doctor.profile.first_name;
+  console.log(first_name);
+  // push to an array? or li each here 
+});
 };
 
 $(document).ready(function() {
